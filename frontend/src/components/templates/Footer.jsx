3f87e1555064dab8f3c78b776e62353449ee6dad
Roselaine Marques <roselaine.marques@liferay.com>
2021-02-03 09:25:26 +0100

    first commit

---
+++ frontend/src/components/templates/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props => 
+    <footer className="footer">
+        <span>
+            Development by <i className="fa fa-heart text-danger"></i> by <strong>Roselaine Marques</strong>
+        </span>
+    </footer>
